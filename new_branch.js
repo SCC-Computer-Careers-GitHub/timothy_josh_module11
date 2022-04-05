49684172afdf5a03e402aec3e45c2fe56f961312
Tim McGinnis <tim.i.mcginnis@gmail.com>
2022-04-04 20:14:14 -0500

    Changes were made to new_branch.js

--- new_branch.js
+++ new_branch.js
@@ -4,14 +4,15 @@
 *
 */
 
-var a = 1;
-var b = 2;
-var c = addition(;
-
 function addition(){
     c = a + b;
 
     return c
 }
 
-console.log(c)
+var a = 1;
+var b = 2;
+var c = addition();
+
+
+console.log(addition())
